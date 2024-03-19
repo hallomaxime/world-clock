9a18f9ecca2f8f381f22a838fec23a68e8d11ba2
Maxime <82838299+hallomaxime@users.noreply.github.com>
2024-03-19 22:00:08 +0100

    js date and time added

---
+++ src/index.js
@@ -0,0 +1,20 @@
+// Los Angeles
+setInterval(setDateTime, 1000);
+
+function setDateTime() {
+  let losAngelesElement = document.querySelector("#los-angeles");
+  let losAngelesDate = losAngelesElement.querySelector(".date");
+  let losAngelesTime = losAngelesElement.querySelector(".time");
+  let losAngelesTimeZone = moment().tz("America/Los_Angeles");
+
+  losAngelesDate.innerHTML = losAngelesTimeZone.format("MMMM Do, YYYY");
+  losAngelesTime.innerHTML = losAngelesTimeZone.format("HH:mm:ss");
+
+  let sydneyElement = document.querySelector("#sydney");
+  let sydneyDate = sydneyElement.querySelector(".date");
+  let sydneyTime = sydneyElement.querySelector(".time");
+  let sydneyTimeZone = moment().tz("Australia/Sydney");
+
+  sydneyDate.innerHTML = sydneyTimeZone.format("MMMM Do, YYYY");
+  sydneyTime.innerHTML = sydneyTimeZone.format("HH:mm:ss");
+}
